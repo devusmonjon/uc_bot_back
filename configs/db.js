5afcf5220cad944438fdf0aef8525ef72e10a318
Azimjon Bekmuratov <76477006+Azimjon1111@users.noreply.github.com>
2025-02-07 14:25:35 +0500

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = "mongodb://localhost:27017/uc_bot";
 
 module.exports = async () => mongoose.connect(mongoURI);
