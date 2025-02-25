9a3a2f3fe783ec9a3329b3c6901a6c3a748a96a0
Xasanov Usmonjon <164606236+devusmonjon@users.noreply.github.com>
2025-02-25 18:03:15 +0500

    Update settings.js

--- constants/settings.js
+++ constants/settings.js
@@ -1,9 +1,9 @@
 const SETTINGS = {
   startmessage: `TILNI TANLANG 🇺🇿 / Выберите язык 🇷🇺`,
   title: "ASLAM UC 24/7 ❄️",
-  support_username: "@DevMERN",
-  market_channel_url: "https://t.me/ucfrozen",
-  reviews_channel_url: "https://t.me/frozenshopreview",
+  support_username: "@cbwwww",
+  market_channel_url: "https://t.me/aslamucservis",
+  reviews_channel_url: "https://t.me/aslamucservis",
   group_id: "-1002343277796",
   webapp_url: "https://uc-bot-webapp.vercel.app/"
 };
