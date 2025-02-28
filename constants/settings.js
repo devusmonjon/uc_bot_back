43ac891d4e313f4f53a6c8da1872b4f69dd7db21
devusmonjon <usmonjonhasanov777@gmail.com>
2025-02-28 11:53:23 +0500

    fix

--- constants/settings.js
+++ constants/settings.js
@@ -2,7 +2,7 @@ const SETTINGS = {
   startmessage: `TILNI TANLANG 🇺🇿 / Выберите язык 🇷🇺`,
   title: "ASLAM UC 24/7 ❄️",
   support_username: "@cbwwww",
-  market_channel_url: "https://t.me/aslamucservis",
+  market_channel_url: "https://t.me/aslamu",
   reviews_channel_url: "https://t.me/aslamucservis",
   group_id: "-1002343277796",
   webapp_url: "https://uc-bot-webapp.vercel.app/"
